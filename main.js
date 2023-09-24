a8b4095e0a828714d8e33c0a858b64f9e9deeb03
I Made Wahyu Artana <141029955+wahyuMWA@users.noreply.github.com>
2023-09-24 20:28:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var navContainer = document.getElementById("navContainer");
+
+function showMenu() {
+  navContainer.style.right = "0";
+}
+
+function hideMenu() {
+  navContainer.style.right = "-200px";
+}
